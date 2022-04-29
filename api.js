585b3c8abcb2f8335545cd2951f8c31f3ce3e580
MatheusJords <matheus_jordan@yahoo.com.br>
2022-04-28 21:14:30 -0300

    bot v1

---
+++ api.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const querystring = require('querystring')
+
+async function publicCall(path,data,method = 'GET'){
+    try{
+        const qs = data ? `?${querystring.stringify(data)}` : '';
+        const result = await axios({
+            method,
+            url:`${process.env.API_URL}${path}${qs}`
+        })
+
+        return result.data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function time(){
+    return publicCall('/v3/time');
+}
+
+async function depth(symbol = 'BTCBRL', limit = 5){
+    return publicCall('/v3/depth',{symbol,limit});
+}
+
+module.exports = { time,depth }
